81dc4aa321756e6a3b8fda4ff979741f064e88b9
Marc Pelland <mpelland@thrillworks.com>
2019-06-24 11:36:36 -0400

    add supporting files for compiling and ignores

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,9 +1,12 @@
-
 const path = require('path');
+
 const fs = require('fs-extra');
 
-const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
-const get = require('lodash/get')
+const {
+  createRemoteFileNode
+} = require(`gatsby-source-filesystem`);
+
+const get = require('lodash/get');
 
 async function copyFiles(node, ext) {
   try {
@@ -11,32 +14,36 @@ async function copyFiles(node, ext) {
     console.info('gatsby-plugin-cdn-files INFO: file moved sucessfully');
     return true;
   } catch (err) {
-    console.error('gatsby-plugin-cdn-files ERROR:', e)
+    console.error('gatsby-plugin-cdn-files ERROR:', e);
     return false;
   }
 }
 
-exports.onCreateNode = async (
-  { node, actions, store, cache, createNodeId },
-  options
-) => {
-  const { createNode } = actions
+exports.onCreateNode = async ({
+  node,
+  actions,
+  store,
+  cache,
+  createNodeId
+}, options) => {
+  const {
+    createNode
+  } = actions;
   const {
     nodeType,
     imagePath,
     name = 'localFile',
     auth = {},
-    ext = null,
-  } = options
+    ext = null
+  } = options;
+  let fileNode;
 
-  let fileNode
   if (node.internal.type === nodeType) {
-
     const filext = imagePath.split('.').pop();
+    const url = ext ? `${get(node, imagePath)}${ext}` : get(node, imagePath);
 
-    const url = ext ? `${get(node, imagePath)}${ext}` : get(node, imagePath)
     if (!url) {
-      return 
+      return;
     }
 
     try {
@@ -48,38 +55,37 @@ exports.onCreateNode = async (
         createNode,
         createNodeId,
         auth,
-        ext,
-      })
+        ext
+      }); // if the extension is listed in the plugin settings
 
-      // if the extension is listed in the plugin settings
       if (ext.includes(filext)) {
         // copy the file
         const copyComplete = copyFiles(fileNode);
+
         if (copyComplete) {
           createNodeField({
             node,
             name: 'path',
-            value: `/static/${ext}/${fileNode.base}`,
+            value: `/static/${ext}/${fileNode.base}`
           });
-
           createNodeField({
             node,
             name: 'ext',
-            value: fileext,
+            value: fileext
           });
         } else {
-                // eslint-disable-next-line no-console
+          // eslint-disable-next-line no-console
           console.error('gatsby-plugin-cdn-files ERROR', 'error copying the file');
         }
       }
-      
     } catch (e) {
-      console.error('gatsby-plugin-cdn-files ERROR:', e)
+      console.error('gatsby-plugin-cdn-files ERROR:', e);
     }
-  }
-  // Adds a field `localImage` or custom name to the node
+  } // Adds a field `localImage` or custom name to the node
   // ___NODE appendix tells Gatsby that this field will link to another node
+
+
   if (fileNode) {
-    node[`${name}___NODE`] = fileNode.id
+    node[`${name}___NODE`] = fileNode.id;
   }
-}
+};
